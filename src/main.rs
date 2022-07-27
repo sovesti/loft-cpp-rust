dd13d52ea60ba7219c32ea3e391b9edd105daa54
Vasily Fedorov <vasek.fedorov@gmail.com>
2022-07-27 22:22:52 +0300

    added cli and moved clang interacting out of main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use crate::json::JSONSerializer;
-use std::{fs::File, io::Write, collections::HashMap};
-use clang::*;
-use graph::*;
-// use parse_cpp::get_tu;
-use std::path::PathBuf;
-use crate::get_name::*;
-use index::collect_entities;
+use std::env;
+use rustop::opts;
+use parse_cpp::parse_trees;
 
 pub mod json;
 pub mod graph;
@@ -14,36 +9,34 @@ pub mod kind;
 pub mod get_name;
 pub mod index;
 
+fn check_slash(path: &mut String) {
+    if !path.ends_with('/') {
+        path.push('/');
+    }
+}
 
-const DIAGNOSTICS : bool = false;
-const EXCLUDE : bool = true;
+fn append_includes(mut parse_options: Vec<String>, mut include_dirs: Vec<String>) -> Vec<String> {
+    include_dirs.append(env::split_paths(&env::var_os("PATH").unwrap())
+    .map(|path| String::from(path.to_str().unwrap())).collect::<Vec<String>>().as_mut());
+    for mut path in include_dirs {
+        check_slash(&mut path);
+        parse_options.push(String::from(String::from("-I") + &path));
+    }
+    parse_options
+}
+
+fn config() -> (Vec<String>, Vec<String>, String) {
+    let (mut args, _) = opts! {
+        opt input_files:Vec<String> = Vec::new(), desc:"Input files.", multi:true;
+        opt output_dir:String=String::from("./"), desc:"Output directory, default is the current";
+        opt include:Vec<String> = Vec::new(), desc:"Include path", multi:true;
+        opt parse_options:Vec<String> = Vec::new(), desc:"Options passed to clang directly", multi:true;
+    }.parse_or_exit();
+    check_slash(&mut args.output_dir);
+    (args.input_files, append_includes(args.parse_options, args.include), args.output_dir)
+}
 
 fn main() {
-    let file = File::create(r"./test.json").unwrap();
-    let mut json = JSONSerializer::new(file);
-    let clang = Clang::new().unwrap();
-    let index = Index::new(&clang, EXCLUDE, DIAGNOSTICS);
-    let parser = index.parser(PathBuf::from(r"C:/work/loft-cpp/src/main.cpp"));
-    let tu = parser.parse().unwrap();
-    for entity in tu.get_entity().get_children() {   
-        let set = collect_entities(entity);
-        for el in set {
-            println!("{}", el);
-        }
-    }
-    let mut ast = AST::new(tu.get_entity());
-    // for entity in ast.nodes {
-    //     println!("{}", entity.get_name().get_name());
-    // }
-    // let mut registry = HashMap::new();
-    // let mut nodes = Vec::<Node>::new();
-    // for entity in ast.nodes {
-    //     let node = Node::new(entity);
-    //     json = Node::new(entity).serialize(json);
-    //     nodes.push(node);
-    // }
-    let node = Node::new(tu.get_entity(), &mut ast).0;
-    json = node.serialize(json);
-    json.writer.flush().unwrap();
-    // parse_cpp::construct_graph(file, output);
+    let (input_files, parse_options, output_dir) = config();
+    parse_trees(input_files, parse_options, output_dir);
 }
--- src/parse_cpp.rs
+++ src/parse_cpp.rs
@@ -1,18 +1,47 @@
-use crate::graph;
+use crate::index::collect_entities;
+use crate::json::JSONSerializer;
 use clang::*;
-use std::path::PathBuf;
-use std::fs::File;
+use std::{fs::*, io::Write, path::PathBuf};
+use crate::graph::*;
 
 const DIAGNOSTICS : bool = true;
 const EXCLUDE : bool = true;
 
-// pub fn get_tu<'a> (path: PathBuf) -> (Clang, Index<'a>, TranslationUnit<'a>, PathBuf) {
-//     let clang = Clang::new().expect("failed to create Clang instance");
-//     let index = Index::new(&clang, EXCLUDE, DIAGNOSTICS);
-//     let parser = index.parser(path);
-//     let tu = parser.parse().expect("failed to parse file");
-//     let path_to_cache = r".\cache\cache.ast";
-//     tu.save(path_to_cache).expect("failed to write to cache");
-//     (clang, index, tu, PathBuf::from(path_to_cache))
-// }
+fn get_parser<'a> (index: &'a Index, path: PathBuf) -> Parser<'a> {
+    index.parser(path)
+}
+
+fn get_tu<'a> (parser: &'a Parser) -> TranslationUnit<'a> {
+    parser.parse().expect("failed to parse file")
+}
+
+fn create_output_file (input_file_path: String, output_dir: String) -> File {
+    create_dir_all(output_dir.clone()).unwrap();
+    let input_file_path_as_path = PathBuf::from(input_file_path);
+    let input_file_path_without_extension = PathBuf::from(input_file_path_as_path.file_stem().unwrap().to_str().unwrap());
+    let output_file_name = input_file_path_without_extension.file_name().unwrap().to_str().unwrap();
+    File::create(output_dir + output_file_name + ".json").unwrap()
+}
+
+pub fn parse_trees (input_files: Vec<String>, mut parse_options: Vec<String>, output_dir: String) {
+    let clang = Clang::new().unwrap();
+    let index = Index::new(&clang, EXCLUDE, DIAGNOSTICS);
+    for path in input_files {
+        let mut parser = get_parser(&index, PathBuf::from(path.clone()));
+        parser.arguments(&mut parse_options);
+        let tu = get_tu(&parser);
+        let out = create_output_file(path, output_dir.clone());
+        let mut json = JSONSerializer::new(out);
+        for entity in tu.get_entity().get_children() {   
+            let set = collect_entities(entity);
+            for el in set {
+                println!("{}", el);
+            }
+        }
+        let mut ast = AST::new(tu.get_entity());
+        let node = Node::new(tu.get_entity(), &mut ast).0;
+        json = node.serialize(json);
+        json.writer.flush().unwrap();
+    }
+}
 
